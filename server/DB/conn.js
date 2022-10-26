51a7fc33aeaf6f57791d6c1bce958ecf047f4967
mujahidhussain313 <89579297+mujahidhussain313@users.noreply.github.com>
2022-10-25 17:33:27 -0700

    Update conn.js

--- server/DB/conn.js
+++ server/DB/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
  const DB = 'mongodb://localhost:27017/dashboard';
  
-//  "mongodb+srv://mujahid:curdapp@cluster0.bynnult.mongodb.net/curdstack?retryWrites=true&w=majority"
+
 
 mongoose.connect(DB, {
     useNewUrlParser: true
